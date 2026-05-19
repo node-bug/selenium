8279b07b0019185a859187e045a0cae59b21aac4
Thomas Dsilva <nodebug@outlook.com>
2026-05-19 22:38:56 +0200

    fixed spatial selectors

--- demo.js
+++ demo.js
@@ -16,15 +16,17 @@ async function test() {
     // await browser.goto('https://seleniumbase.io/demo_page');
     // await browser.checkbox('checkBox6').check();
 
-        await browser.goto('file:///Users/thomasdsilva/Projects/selenium/tests/fixtures/alerts.html');
+    await browser.goto('file:///Users/thomasdsilva/Projects/selenium/tests/fixtures/spatial-test.html');
 
-    await browser.element('Click for JS Prompt').click();
-    await browser.alert('I am a JS prompt').is.visible();
-    await browser.alert().write('Hello World');
-    await browser.alert().accept();
-    // Verify the result text
-    await browser.exact.element('result').get.text();
-    // expect(resultText).toContain('You entered: Hello World');
+    await browser
+            .element('Top Right Corner')
+            .toRightOf.element('Left Edge')
+            .should.be.visible();
+
+    await browser
+            .element('Top Right Corner')
+            .exactly.toRightOf.element('Left Edge')
+            .should.be.visible();
 
     await browser.close();
 }
